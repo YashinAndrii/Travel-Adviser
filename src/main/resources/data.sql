1e8c43e5f888d9832adf493135d9dc7599dc81e6
Yashyn, Andrii [PS] <andrii.yashyn@cengage.com>
2025-07-04 18:54:17 +0300

    Changed init data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -72,10 +72,10 @@ CREATE TABLE comments
 
 -- USERS
 INSERT INTO users (id, username, display_name, avatar_url, bio, created_at)
-VALUES ('11111111-1111-1111-1111-111111111111', 'travel_anna', 'Anna Traveler', 'https://example.com/avatar1.jpg',
+VALUES ('11111111-1111-1111-1111-111111111111', 'travel_anna', 'Anna Traveler', 'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?w=150&h=150&fit=crop&crop=face',
         'Exploring the world one trip at a time.', CURRENT_TIMESTAMP),
        ('22222222-2222-2222-2222-222222222222', 'globetrotter_bob', 'Bob Globetrotter',
-        'https://example.com/avatar2.jpg', 'Adventure is out there!', CURRENT_TIMESTAMP);
+        'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=150&h=150&fit=crop&crop=face', 'Adventure is out there!', CURRENT_TIMESTAMP);
 
 -- POSTS
 INSERT INTO posts (id, user_id, dates, type, budget, is_planned, created_at)
@@ -84,7 +84,7 @@ VALUES ('aaaaaaa1-aaaa-aaaa-aaaa-aaaaaaaaaaa1', '11111111-1111-1111-1111-1111111
 
 -- POST_PHOTOS
 INSERT INTO posts_photos (post_id, photo_url)
-VALUES ('aaaaaaa1-aaaa-aaaa-aaaa-aaaaaaaaaaa1', 'https://example.com/photo1.jpg');
+VALUES ('aaaaaaa1-aaaa-aaaa-aaaa-aaaaaaaaaaa1', 'https://images.unsplash.com/photo-1433086966358-54859d0ed716?w=800&h=1000&fit=crop');
 
 -- POST_COUNTRIES
 INSERT INTO posts_countries (post_id, country)
@@ -95,3 +95,42 @@ VALUES ('aaaaaaa1-aaaa-aaaa-aaaa-aaaaaaaaaaa1', 'Italy'),
 INSERT INTO posts_cities (post_id, city)
 VALUES ('aaaaaaa1-aaaa-aaaa-aaaa-aaaaaaaaaaa1', 'Rome'),
        ('aaaaaaa1-aaaa-aaaa-aaaa-aaaaaaaaaaa1', 'Paris');
+
+-- Второй пост: активное приключение
+INSERT INTO posts (id, user_id, dates, type, budget, is_planned, created_at)
+VALUES ('aaaaaaa2-aaaa-aaaa-aaaa-aaaaaaaaaaa2', '22222222-2222-2222-2222-222222222222', 'July 15 - July 25, 2025',
+        'ADVENTURE', '$1800', FALSE, CURRENT_TIMESTAMP);
+
+INSERT INTO posts_photos (post_id, photo_url)
+VALUES ('aaaaaaa2-aaaa-aaaa-aaaa-aaaaaaaaaaa2', 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?w=800&h=1000&fit=crop');
+
+INSERT INTO posts_countries (post_id, country)
+VALUES ('aaaaaaa2-aaaa-aaaa-aaaa-aaaaaaaaaaa2', 'Switzerland');
+
+INSERT INTO posts_cities (post_id, city)
+VALUES ('aaaaaaa2-aaaa-aaaa-aaaa-aaaaaaaaaaa2', 'Zermatt');
+
+-- Лайки ко второму посту
+INSERT INTO likes (id, user_id, post_id, created_at)
+VALUES ('bbbbbbb1-bbbb-bbbb-bbbb-bbbbbbbbbbb1', '11111111-1111-1111-1111-111111111111', 'aaaaaaa2-aaaa-aaaa-aaaa-aaaaaaaaaaa2', CURRENT_TIMESTAMP);
+
+-- Третий пост: запланированная культурная поездка
+INSERT INTO posts (id, user_id, dates, type, budget, is_planned, created_at)
+VALUES ('aaaaaaa3-aaaa-aaaa-aaaa-aaaaaaaaaaa3', '11111111-1111-1111-1111-111111111111', 'August 10 - August 20, 2025',
+        'CULTURAL', '$2200', TRUE, CURRENT_TIMESTAMP);
+
+INSERT INTO posts_photos (post_id, photo_url)
+VALUES ('aaaaaaa3-aaaa-aaaa-aaaa-aaaaaaaaaaa3', 'https://images.unsplash.com/photo-1526772662000-3f88f10405ff?w=800&h=1000&fit=crop');
+
+INSERT INTO posts_countries (post_id, country)
+VALUES ('aaaaaaa3-aaaa-aaaa-aaaa-aaaaaaaaaaa3', 'Japan');
+
+INSERT INTO posts_cities (post_id, city)
+VALUES ('aaaaaaa3-aaaa-aaaa-aaaa-aaaaaaaaaaa3', 'Kyoto');
+
+-- Лайки к третьему посту
+INSERT INTO likes (id, user_id, post_id, created_at)
+VALUES
+    ('bbbbbbb2-bbbb-bbbb-bbbb-bbbbbbbbbbb2', '22222222-2222-2222-2222-222222222222', 'aaaaaaa3-aaaa-aaaa-aaaa-aaaaaaaaaaa3', CURRENT_TIMESTAMP),
+    ('bbbbbbb3-bbbb-bbbb-bbbb-bbbbbbbbbbb3', '11111111-1111-1111-1111-111111111111', 'aaaaaaa3-aaaa-aaaa-aaaa-aaaaaaaaaaa3', CURRENT_TIMESTAMP);
+
